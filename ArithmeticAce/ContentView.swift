72d3be2ceaab12df1d1a711e832200e5d7cfc20a
Madison Dellamea <mdellamea@lcs.on.ca>
2022-02-08 12:03:50 -0500

    Added @State property wrapper to both multiplicand and multiplier variables. Added new variable "newQuestion" with property wrapper. Made sure that the green checkmark image drops to 0.0 opacity whenever newQuestion is set to true. Added x.circle image when answerCorrect == false.

--- ArithmeticAce/ContentView.swift
+++ ArithmeticAce/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     // MARK: Stored properties
     @State var multiplicand = Int.random(in: 1...12)
     @State var multiplier = Int.random(in: 1...12)
+    
+    //This string contains whatever the user types in.
     @State var inputGiven = ""
     @State var newQuestion = false
     
@@ -28,7 +30,7 @@ struct ContentView: View {
     }
     
     var body: some View {
-                
+        
         VStack(spacing: 0) {
             HStack {
                 Text("✕")
@@ -44,69 +46,89 @@ struct ContentView: View {
             Divider()
             
             HStack {
-                Image(systemName: "checkmark.circle")
-                    .foregroundColor(.green)
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
                     //        CONDITION      true  false
-                    .opacity(answerCorrect ? 1.0 : 0.0)
-                    .opacity(newQuestion ? 1.0 : 0.0)
-                Spacer()
-                TextField("",
-                          text: $inputGiven)
-                    .multilineTextAlignment(.trailing)
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                        .opacity(newQuestion ? 1.0 : 0.0)
+                    Image(systemName: "x.circle")
+                        .foregroundColor(.red)
+                    //        CONDITION      true  false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    
+                    
+                    Spacer()
+                    TextField("",
+                              text: $inputGiven)
+                        .multilineTextAlignment(.trailing)
+                }
             }
             
             //Check Answer Button
             
-            Button(action: {
+            ZStack {
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+                    
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's not correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                //Only show this button when an answer has not been checked.
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
                 
-                // Answer has been checked!
-                answerChecked = true
+                //New Question Button
                 
-                // Convert the input given to an integer, if possible
-                guard let productGiven = Int(inputGiven) else {
-                    // Sadness, not a number
+                Button(action: {
+                    
+                    // New Quesetion!
+                    newQuestion = true
+                    
+                    //Reset properties that check what's happening with the current question.
+                    answerChecked = false
                     answerCorrect = false
-                    return
-                }
-
-                // Check the answer!
-                if productGiven == correctProduct {
-                    // Celebrate! 👍🏼
-                    answerCorrect = true
-                } else {
-                    // Sadness, they gave a number, but it's not correct 😭
-                    answerCorrect = false
-                }
-            }, label: {
-                Text("Check Answer")
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
-            
-            //New Question Button
-            
-            Button(action: {
-                
-                // New Quesetion!
-                newQuestion = true
-
-               inputGiven = ""
-                
-                multiplicand = Int.random(in: 1...12)
-                multiplier = Int.random(in: 1...12)
-                
-                
-            }, label: {
-                Text("New Question")
-                    .font(.largeTitle)
-            })
-                .padding()
-                .buttonStyle(.bordered)
+                    
+                    //Reset the inuput field
+                    inputGiven = ""
+                    
+                    //New Question
+                    multiplicand = Int.random(in: 1...12)
+                    multiplier = Int.random(in: 1...12)
+                    
+                    
+                }, label: {
+                    Text("New Question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                //Only show this button when an answer has been checked.
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+            }
         }
         .padding(.horizontal)
         .font(.system(size: 72))
-
+        
         
     }
 }
